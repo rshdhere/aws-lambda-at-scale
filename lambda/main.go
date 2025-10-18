02d81e9d7e08193c0da22a425fb620244451bfa6
raashed <mohammedraashed10@gmail.com>
2025-10-18 08:53:42 +0530

    feat: basic lambda deployment

--- lambda/main.go
+++ lambda/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+// this will take in some payload and do some process w it
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username cannot be empty")
+	}
+
+	return fmt.Sprintf("successfully called %s", event.Username), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
 }
