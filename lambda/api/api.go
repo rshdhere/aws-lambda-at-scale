8b9813771064e9d8b243f99b740da0e0a35a0fbd
raashed <mohammedraashed10@gmail.com>
2025-10-18 17:29:01 +0530

    feat: added basic api layer

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import "lambda-func/database"
+
+type ApiHandler struct {
+	dbStore database.DynamoDBClient
+}
+
+func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
+	return ApiHandler{
+		dbStore: dbStore,
+	}
+}
