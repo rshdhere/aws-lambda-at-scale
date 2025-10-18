0bd890c9c189db1f39d8f928ead78bfb913f3325
raashed <mohammedraashed10@gmail.com>
2025-10-18 16:57:19 +0530

    feat: added basic database struct

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,1 +1,11 @@
 package database
+
+type DynamoDBClient struct {
+	databaseStore string
+}
+
+func DynamoDBClientFunction() DynamoDBClient {
+	return DynamoDBClient{
+		databaseStore: "database store",
+	}
+}
