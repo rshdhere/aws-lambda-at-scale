a1a19f8dfea20a994fa5e448eecc2afa05f05ce0
raashed <mohammedraashed10@gmail.com>
2025-10-19 15:18:52 +0530

    feat: added check if the user exists or not

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,8 +1,13 @@
 package database
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+const (
+	TABLE_NAME = "userTable"
 )
 
 type DynamoDBClient struct {
@@ -17,4 +22,25 @@ func NewDynamoDBClient() DynamoDBClient {
 	return DynamoDBClient{
 		databaseStore: db,
 	}
+}
+
+func (u DynamoDBClient) DoesUserExists(username string) (bool, error) {
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+
+	if err != nil {
+		return true, err
+	}
+
+	if result.Item == nil {
+		return false, nil
+	}
+
+	return true, nil
 }
